a5aff9fdc350b56217f4f0d72821d7591dacefd8
RutNia <ruth.benitez.98@est.ucacue.edu.ec>
2026-07-21 11:50:49 -0500

    v1.0 - Creación inicial del archivo y estructura del proyecto

---
+++ login.py
@@ -0,0 +1,11 @@
+"""
+PagoSimple - Sistema Transaccional
+Modulo de Login y Seguridad
+Responsable del proyecto: [Tu nombre] (Lider de Proyecto)
+"""
+
+def main():
+    print("Sistema PagoSimple en construccion...")
+
+if __name__ == "__main__":
+    main()
